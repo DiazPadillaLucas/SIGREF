3d54897460985c228e0960c54d5e127b9a340c60
MirianBaigorria <mirianbaigo@gmail.com>
2025-10-05 21:16:05 -0300

    agragando movimiento de hoy del inicio sin funcionar

--- src/main/java/com/sistema/demo/repositorio/MovimientoRepositorio.java
+++ src/main/java/com/sistema/demo/repositorio/MovimientoRepositorio.java
@@ -5,10 +5,15 @@ import com.sistema.demo.entidad.Recurso;
 import com.sistema.demo.entidad.enums.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
 
 import java.util.List;
 
 public interface MovimientoRepositorio extends JpaRepository<Movimiento, Long> {
     List<Movimiento> findAllByOrderByFechaDesc(Pageable pageable);
+    @Query("SELECT COUNT(m) FROM Movimiento m WHERE FUNCTION('DATE', m.fecha) = CURRENT_DATE")
+    long contarMovimientosDeHoy();
 }
